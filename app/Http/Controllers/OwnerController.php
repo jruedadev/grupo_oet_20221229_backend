b6c5abfd07d8e304698e7cdbb9c4d685649e5f34
Johan Rueda <librajohan2000@gmail.com>
2022-12-29 00:30:58 -0500

    Ajustes Modelo Vehiculos

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -17,7 +17,7 @@ class OwnerController extends BaseController
      */
     public function index(Request $request)
     {
-        return response()->json(Owner::get(), 200);
+        return response()->json(Owner::with('vehicle')->get(), 200);
     }
 
     /**
